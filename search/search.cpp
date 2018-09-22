6b7a132dbd5c366941cf21d3c1ab91e440204766
Angel Lopez Manriquez <sigma271@outlook.com>
2018-09-21 23:26:21 -0500

    Add files via upload

--- search/search.cpp
+++ search/search.cpp
@@ -6,6 +6,7 @@
 #include <iterator> // definicion de ostream_iterator
 #include <algorithm> // funcion generica sort
 #include <chrono> // medicion de tiempo
+#include <typeinfo> // obtencion del tipo de dato de una variable
 #include <initializer_list> // argumentos variables de c++
 
 // bibliotecas necesarias para la programacion concurrente
@@ -22,53 +23,18 @@
 #include "bst.h"
 #include "files.hpp"
 
-const size_t NTHREADS = 32;
+const size_t NTHREADS = 4;
 
 using namespace std;
 
 typedef pair<int, int> int_pair;
 
 /**
- * Determina si x pertenece a un vector retornando un booleano.
- * Esta es la forma mas trivial y lenta de buscar elementos en una coleccion, tiene
- * complejidad O(n).
+ * Funcion que sobrecarga el operador de flujo de salida con el fin de que cualquier 
+ * vector de tipo t pueda ser impreso usando cout.
+ * 
+ * Nota: C++ de alguna manera tiene que saber como imprimir al tipo t.
  */
-bool linear_search(const vector<int> &v, const int &x, int_pair p = make_pair(-1, -1)) {
-    int a = p.first, b = p.second;
-    if (p == make_pair(-1, -1)) // se procedera a buscar sobre todo el vector
-        a = 0, b = v.size() - 1;
-    for (int i = a; i <= b; i++)
-        if (v[i] == x) return true;
-    return false;
-}
-
-/**
- * Determina si x pertenece a un vector previamente ordenado. Se reinventa la rueda pues
- * ya existe binary_search en la STL de C++.
- * Para calcular la mitad del rango es mejor calcular l+(r-l)/2 a comparacion de (l+r)/2
- * ya que l+r puede albergar un entero muy grande.
- */
-bool bin_search_helper(const vector<int> &v, const int &x, int left, int right) {
-    if (left > right) return false;
-    int i = left + (right - left) / 2;
-    if (v[i] == x) return true;
-    if (x < v[i]) return bin_search_helper(v, x, left, --i);
-    return bin_search_helper(v, x, ++i, right);
-}
-
-/**
- * Funcion de apoyo que manda a llamar a la misma sobrecargada. La complejidad del
- * algoritmo es O(n lg n).
- */
-bool bin_search(const vector<int> &v, const int findable, 
-        const int_pair lims = make_pair(-1, -1)) {
-    int a = lims.first, b  = lims.second;
-    if (a == -1 && b == -1)
-        a = 0, b = v.size() - 1;
-    return bin_search_helper(v, findable, a, b);
-}
-
-
 template <typename T>
 std::ostream& operator<< (std::ostream& out, const std::vector<T>& v) {
   if ( !v.empty() ) {
@@ -87,52 +53,84 @@ std::ostream& operator<< (std::ostream& out, const std::vector<T>& v) {
  * 50 // 4 = 12
  * (0, 11), (12, 23), (24, 35), (36, 47 + 2)
  *
- * por supuesto, n > k.
+ * por supuesto, se debe tener n > k.
  */
 vector<pair<int, int>> intervals(const int n, const int k) {
     int quotient = n / k, j = 0;
-    vector<pair<int, int>> pairs;
+    vector<int_pair> pairs;
     pairs.reserve(k);
+
+    // es mas rapida la funcion miembro emplace_back que push_back, esta manda a llamar 
+    // al contructor definido en el operador diamante
+    // pairs.push_back(make_pair(j, j + quotient - 1));
     for (int i = 0; i < k - 1; i++, j += quotient) {
-        pairs.push_back(make_pair(j, j + quotient - 1));
+        pairs.emplace_back(j, j + quotient - 1);
     }
-    pairs.push_back(make_pair(j, n - 1));
+    pairs.emplace_back(j, n - 1);
     return pairs;
 }
 
+/**
+ * Determina si x pertenece a un vector retornando un booleano.
+ * Esta es la forma mas trivial y lenta de buscar elementos en una coleccion, tiene
+ * complejidad O(n).
+ */
+bool linear_search(const vector<int> &v, const int &x, int_pair p = make_pair(-1, -1)) {
+    int a = p.first, b = p.second;
+    if (p == make_pair(-1, -1)) // se procedera a buscar sobre todo el vector
+        a = 0, b = v.size() - 1;
+    for (int i = a; i <= b; i++)
+        if (v[i] == x) return true;
+    return false;
+}
+
 bool concurrent_linear_search(const vector<int> &v, const int &value) {
-  bool found = false;
   vector<future<bool>> futures;
   futures.reserve(NTHREADS);
   vector<int_pair> pairs = intervals(v.size(), NTHREADS);
-  for (int i = 0; i < NTHREADS; i++) {
+  for (int i = 0; i < NTHREADS; ++i) {
     futures.push_back( async(linear_search, v, value, pairs[i]) );
   }
-  for (int i = 0; i < NTHREADS; i++) {
-      found = found or futures[i].get();
-  }
-  return found;
+  for (int i = 0; i < NTHREADS; ++i) 
+      if (futures[i].get()) return true;
+  return false;
+}
+
+/**
+ * Determina si x pertenece a un vector previamente ordenado. Se reinventa la rueda pues
+ * ya existe binary_search en la STL de C++.
+ * Para calcular la mitad del rango es mejor calcular l+(r-l)/2 a comparacion de (l+r)/2
+ * ya que l+r puede albergar un entero muy grande.
+ */
+bool bin_search_helper(const vector<int> &v, const int &x, int left, int right) {
+    if (left > right) return false;
+    int i = left + (right - left) / 2;
+    cout << v[i] << endl;
+    if (v[i] == x) return true;
+    if (x < v[i]) return bin_search_helper(v, x, left, --i);
+    return bin_search_helper(v, x, ++i, right);
+}
+
+/**
+ * Funcion de apoyo que manda a llamar a la misma sobrecargada. La complejidad del
+ * algoritmo es O(n lg n).
+ * 
+ * Para que el algoritmo funcione v tiene que estar ordenado.
+ */
+bool bin_search(const vector<int> &v, const int findable, 
+        const int_pair lims = make_pair(-1, -1)) {
+    int a = lims.first, b  = lims.second;
+    if (a == -1 && b == -1)
+        a = 0, b = v.size() - 1;
+    return bin_search_helper(v, findable, a, b);
 }
 
 /*
-vector<vector<int>> partitionate(vector<int> &src, int npartitions) {
-    auto pairs = intervals(src.size(), npartitions);
-    vector<vector<int>> partitioned;
-    partitioned.reserve(npartitions);
-    for (int i = 0; i < npartitions; ++i) {
-        partitioned[i].emplace_back((src.cbegin() + pairs[i].f(rst, src.cbegin() + pairs[i].second));
-    }
-    return partitioned;
-}
-*/
-
-
 bool conc_bin_search_helper(const vector<int> &v, const int &value, 
                             const vector<int_pair> &pairs) {
     bool found = false;
     vector<future<bool>> futures;
     futures.reserve(NTHREADS);
-    vector<int_pair> pairs = intervals(v.size(), NTHREADS);
     for (int i = 0; i < NTHREADS; i++) 
         futures.push_back( async(bin_search, v, value, pairs[i]) );
     for (int i = 0; i < NTHREADS; i++) 
@@ -146,28 +144,42 @@ bool concurrent_binary_search(const vector<int> v, const int &value) {
         sort(v.begin() + p.first, v.begin() + p.second + 1);
     return conc_bin_search_helper(v, value, pairs);
 }
+*/
 
-void measure_function()
 
-int main(int argc, char const *argv[]) {
-    vector<int> v = read_from_file("numeros10millones.txt", 10e6);
-    //vector<int> v = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    //vector<int> v = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-    //vector<int> v; 
-	//cout << v << endl;
-    sort(v.begin(), v.end()); 
-    // medicion del tiempo en ejecucion
+/**
+ * Funcion lambda tipo wrapper para poder medir el tiempo en ejecucion de cualquier funcion.
+ * Puede que el tiempo en ejecucion incrementa ligeramente por el uso de este wrapper.
+ * No puede haber parametros por defecto para la funcion en este wrapper, si se requiere
+ * hay que ponerlos explicitamente.
+ */
+auto timeFuncInvocation = [](auto&& func, auto&&... params) {
+    bool found; // variable a regresar
+    cout << endl;
     chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
-    cout << bin_search(v, 218639060) << endl;
+
+    // aplicamos la funcion con los parametros dados
+    found = std::forward<decltype(func)>(func)(std::forward<decltype(params)>(params)...); 
+
     chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
     auto duration = chrono::duration_cast<chrono::microseconds>( t2 - t1 ).count();
-    cout << duration << endl;
+    cout << "Tiempo de ejecucion en microsegundos: " << duration << endl;
+    return found;
+};
 
-    //t1 = chrono::high_resolution_clock::now();
-    //cout << bin_search(v, 218639060) << endl;
-    //t2 = chrono::high_resolution_clock::now();
-    //duration = chrono::duration_cast<chrono::microseconds>( t2 - t1 ).count();
-    //cout << duration << endl;
+int main(int argc, char const *argv[]) {
+    bool found;
+    vector<int> v = read_from_file("numeros10millones.txt", 10e6);
+    cout << endl;
+
+    //vector<int> v = read_from_file("10e3sorted.txt", 10e6);
+    //vector<int> v = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    found = timeFuncInvocation(linear_search, v, 2045206161, make_pair(-1, -1)) ;
+    cout << found << endl;
+
+    found = timeFuncInvocation(concurrent_linear_search, v, 2045206161) ;
+    cout << found << endl;
 
     cout << endl;
     return 0;
